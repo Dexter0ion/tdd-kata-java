0aee9cb5e1a4dd7a4ec39c7a48fe2f092c94595a
邵博超DEXION <dexter0ion@gmail.com>
2017-06-02 23:14:04 +0800

    [M001][Shane&Water] - add the collection library

---
+++ src/test/java/org/coach/tdd/kata/FizzBuzzTest.java
@@ -0,0 +1,54 @@
+package org.coach.tdd.kata;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void shouldReturnTheNumberItselfGivenTheNumberCannotBeDivedByThreeOrFive() throws Exception {
+        assertThat(fizzBuzz.calculate(1), is("1"));
+    }
+
+    @Test
+    public void shouldReturnFizzGivenTheNumberCanBeDividedByThree() throws Exception {
+        assertThat(fizzBuzz.calculate(3), is("Fizz"));
+        assertThat(fizzBuzz.calculate(6), is("Fizz"));
+        assertThat(fizzBuzz.calculate(9), is("Fizz"));
+    }
+
+    @Test
+    public void shouldReturnBuzzGiveTheNumberCanBeDividedByFive() throws Exception {
+        assertThat(fizzBuzz.calculate(5), is("Buzz"));
+        assertThat(fizzBuzz.calculate(10), is("Buzz"));
+        assertThat(fizzBuzz.calculate(20), is("Buzz"));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzGivenTheNumberCanBeDividedByBothThreeAndFive() throws Exception {
+        assertThat(fizzBuzz.calculate(15), is("FizzBuzz"));
+        assertThat(fizzBuzz.calculate(30), is("FizzBuzz"));
+        assertThat(fizzBuzz.calculate(45), is("FizzBuzz"));
+        assertThat(fizzBuzz.calculate(60),is("FizzBuzz"));
+    }
+
+    @Test(expected = InvalidException.class)
+    public void shouldThrowInvalidNumberExceptionGivenTheNumberIsZero() throws Exception {
+        fizzBuzz.calculate(0);
+    }
+
+    @Test(expected = InvalidException.class)
+    public void shouldThrowInvalidNumberExceptionGivenTheNumberIs() throws Exception {
+        fizzBuzz.calculate(-1);
+    }
+}
